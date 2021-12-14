a8d0ba876d7fd8bb6f914b72953bf2707fcee785
Gitea <gitea@fake.local>
2021-12-14 15:45:26 +0100

    Added download function

--- index.mjs
+++ index.mjs
@@ -1,11 +1,41 @@
 import WebSocket, { WebSocketServer } from "ws";
+import express from 'express';
+import { exec } from 'child_process' ;
+import path from 'path'
+import contentDisposition from 'content-disposition'
+const app = express()
+const port = 8082
+const port_ws = 8081
+const __dirname = path.resolve();
 
+
+/**
+ * Enables the automatic downloading feature, requires yt-dlp
+ */
+app.get('/down/:naslov', (req, res) => {
+	const ytdlp = `yt-dlp ytsearch:"${req.params.naslov}" -x -f m4a --sponsorblock-remove all -o 'audio/%(title)s.%(ext)s' --no-playlist --no-simulate --print 'audio/%(title)s.%(ext)s' --no-progress`
+	exec(ytdlp, (err, stdout, stderr) => {
+		if (err) {
+			res.send(err +"\n"+ req.params +"\n"+ stderr)
+		} else {
+			let filename = stdout.trim()
+			// trim() removes /n on stdout
+			res.set('Content-Disposition', contentDisposition(filename))
+			res.sendFile(filename, {root: __dirname})
+		}
+	})
+
+})
+
+app.listen(port, () => {
+	console.log(`express starting on${port}`)
+})
 let db = []
     /**
      * Set the port here
      */
 const wss = new WebSocketServer({
-    port: 8080
+    port: port_ws
 });
 console.log("started")
 
